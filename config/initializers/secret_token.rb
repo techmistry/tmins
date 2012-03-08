4114589aa30ce5115e57af86b35d317ffe9a140d
Tech Mistry <techmistry@gmail.com>
2012-03-08 12:40:39 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tmins::Application.config.secret_token = '63d7227798915c76d424ca13d3bb158fb2f6b5805d605859df688177e237f73a982da4e749e7331d23555913cbbb7163564ecd219ae56b1ec265ebfa05973717'
